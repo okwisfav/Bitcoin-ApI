23ae6ba4016839cb45b2b8578fadd9c315c0ee17
ofego favour <ofegofavour@gmail.com>
2022-08-27 13:12:10 +0100

    Featured

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 import BTC from '../asset/btc-img.png';
 import {FiArrowUpRight, FiArrowUpDown} from 'react-icons/fi'
 import './Featured.css'
+import axios from 'axios';
 const Featured = () => {
+    const {data, setData} = useState(null)
+    const url ='https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=6&page=1&sparkline=false'
+    useEffect(() =>{
+       axios.get(url).then((response) =>{
+         setData(response.data)
+       }).catch((error) => {
+         console.log(error)
+       })
+    }, [])
   return (
     <div className='featured'>
         <div className='container'>
