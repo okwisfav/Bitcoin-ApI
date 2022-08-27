beb0cbb61faaaf7d332ac618254cb8dc372f1501
ofego favour <ofegofavour@gmail.com>
2022-08-27 14:12:15 +0100

    FEATURED

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -1,18 +1,25 @@
 import React,{useState, useEffect} from 'react'
-import BTC from '../asset/btc-img.png';
-import {FiArrowUpRight, FiArrowUpDown} from 'react-icons/fi'
+// import BTC from '../asset/btc-img.png';
+import {FiArrowUpRight, FiArrowUpDown, FiArrowDown} from 'react-icons/fi'
 import './Featured.css'
 import axios from 'axios';
 const Featured = () => {
-    const {data, setData} = useState(null)
-    const url ='https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=6&page=1&sparkline=false'
-    useEffect(() =>{
-       axios.get(url).then((response) =>{
-         setData(response.data)
-       }).catch((error) => {
-         console.log(error)
-       })
+    const [data, setData] = useState(null)
+
+    const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=6&page=1&sparkline=false'
+
+    useEffect(() => {
+        axios.get(url).then((response) => {
+            setData(response.data)
+        }).catch((error) => {
+            console.log(error)
+        })
     }, [])
+
+    console.log(data)
+    if(!data) return null
+
+    if (!data) return null
   return (
     <div className='featured'>
         <div className='container'>
@@ -25,13 +32,25 @@ const Featured = () => {
             {/* Right */}
             <div className='right'>
                  <div className='top'>
-                     <img src={BTC} alt="/" />
+                     {/* <img src={BTC} alt="/" /> */}
+                     <img src={data[0].image} alt='' />;
                  </div>
                  <div>
-                     <h5>Bitcoin</h5>
-                     <p>20,456</p>
+                     <h5>{data[0].name}</h5>
+                     <p>${data[0].current_price.toLocaleString()}</p>
                  </div>
-                 <span><FiArrowUpRight/>2.5%</span>
+                 {data[0].price_change_percentage_24h < 0 ? (
+                            <span className='red'>
+                                <FiArrowDown className='icon' />
+                                {data[0].price_change_percentage_24h.toFixed(2)}%
+                            </span>
+                        ) : (
+                                <span className='green'>
+                                    <FiArrowUpRight className='icon' />
+                                    {data[0].price_change_percentage_24h.toFixed(2)}%
+                                </span>
+                            )}
+                 {/* <span><FiArrowUpRight/>2.5%</span> */}
             </div>
         </div>
     </div>
